4e4940dbc5facd472cb94fa0652ac189a2b4a5e0
vikkitorry <vikkitorry6716@gmail.com>
2024-01-15 15:43:28 +0700

    fix: appRoutes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,3 @@
-import { Outlet } from 'react-router-dom';
-import classes from './layout.module.scss';
-import Header from '../components/header/Header';
-import Footer from '../components/Footer/Footer';
-import { TranslatorContext } from '../context/translatorContextProvider';
-import { translationData } from '../context/translationData/translationData';
-import { useState } from 'react';
-import { Language } from '../context/translatorContextProvider';
-import Loader from '../components/Loader/Loader';
-
 type LayoutProps = {
   userLoggedIn: boolean;
   setUserLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,24 +5,7 @@ type LayoutProps = {
 };
 
 const Layout = (props: LayoutProps) => {
-  const { userLoggedIn, setUserLoggedIn, isLoading } = props;
-  const [lang, setLang] = useState<Language>('en');
-
-  return !isLoading ? (
-    <TranslatorContext.Provider value={{ lang, data: translationData, setLang }}>
-      <div className={classes.wrapper}>
-        <Header userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} />
-        <main className={classes.main}>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </TranslatorContext.Provider>
-  ) : (
-    <div className={classes.wrapper}>
-      <Loader />
-    </div>
-  );
+  return <div>Layout</div>;
 };
 
 export default Layout;
