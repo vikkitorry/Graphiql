b5668765087813224655eaa67c3494ad0f24266b
vikkitorry <vikkitorry6716@gmail.com>
2024-01-17 15:36:25 +0700

    feat: graphi

---
+++ src/components/Documentation.tsx/Documentation.tsx
@@ -0,0 +1,11 @@
+import cls from './documentation.module.scss';
+
+const Documentation = () => {
+  return (
+    <div className={cls.container} data-testid="documentation">
+      Documentation
+    </div>
+  );
+};
+
+export default Documentation;
