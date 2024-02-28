090b769bb6ed0837cfc05c43faeae881f3989f3f
vikkitorry <vikkitorry6716@gmail.com>
2024-02-28 13:55:38 +0700

    refactor: welcome styles

--- src/components/WelcomeInfo/WelcomeInfo.tsx
+++ src/components/WelcomeInfo/WelcomeInfo.tsx
@@ -10,13 +10,13 @@ const WelcomeInfo = () => {
     <section className={cls.container}>
       <h2 className={cls.tittle}>{data[lang].welcomeTittle}</h2>
       <p className={cls.text}>{data[lang].welcomeTittleText}</p>
-      <div className={cls.blockLight}>
+      <div className={`${cls.blockLight} ${cls.block}`}>
         <div className={cls.imgContainer}>
           <img src={cat} className={cls.cat} />
         </div>
         <p className={cls.desc}>{data[lang].welcomeAboutTool}</p>
       </div>
-      <div className={cls.blockDark}>
+      <div className={`${cls.blockDark} ${cls.block}`}>
         <p className={cls.desc}>{data[lang].welcomeAboutProject}</p>
         <div className={cls.imgContainer}>
           <div className={cls.hexagon}></div>
@@ -27,9 +27,9 @@ const WelcomeInfo = () => {
       <h2 className={cls.tittle}>{data[lang].mobileVersion}</h2>
       <div className={cls.mobileContainer}>
         <div className={cls.mobileSection}>
-          <img src={screen} className={cls.cat} />
-          <img src={screen} className={cls.cat} />
-          <img src={screen} className={cls.cat} />
+          <img src={screen} />
+          <img src={screen} />
+          <img src={screen} />
         </div>
       </div>
     </section>
